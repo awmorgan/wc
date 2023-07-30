eb0b18d38d56223f1dc4802d9822ae5514915af3
Bill Morgan <arthurwilliammorgan@gmail.com>
2023-07-30 07:09:56 -0500

    add -b for bytes, refactor, update tests

--- main.go
+++ main.go
@@ -10,18 +10,38 @@ import (
 
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count bytes")
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines))
+	var fn bufio.SplitFunc
+	switch {
+	case *lines && *bytes:
+		fmt.Println("Cannot use both -l and -b")
+		os.Exit(1)
+	case *lines:
+		fn = bufio.ScanLines
+	case *bytes:
+		fn = bufio.ScanBytes
+	default:
+		fn = bufio.ScanWords
+	}
+	wc, err := count(os.Stdin, fn)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(wc)
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, fn bufio.SplitFunc) (int, error) {
 	scanner := bufio.NewScanner(r)
-	if !countLines {
-		scanner.Split(bufio.ScanWords)
-	}
+	scanner.Split(fn)
 	wc := 0
 	for scanner.Scan() {
 		wc++
 	}
-	return wc
+	if err := scanner.Err(); err != nil {
+		return wc, err
+	}
+	return wc, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"testing"
 )
@@ -8,17 +9,34 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
-	res := count(b, false)
-	if res != exp {
-		t.Errorf("Expected %d, got %d", exp, res)
+	got, err := count(b, bufio.ScanWords)
+	if err != nil {
+		t.Error(err)
+	}
+	if got != exp {
+		t.Errorf("Expected %d, got %d", exp, got)
 	}
 }
 
 func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
 	exp := 3
-	res := count(b, true)
-	if res != exp {
-		t.Errorf("Expected %d, got %d", exp, res)
+	got, err := count(b, bufio.ScanLines)
+	if err != nil {
+		t.Error(err)
+	}
+	if got != exp {
+		t.Errorf("Expected %d, got %d", exp, got)
+	}
+}
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("12345")
+	exp := 5
+	got, err := count(b, bufio.ScanBytes)
+	if err != nil {
+		t.Error(err)
+	}
+	if got != exp {
+		t.Errorf("Expected %d, got %d", exp, got)
 	}
 }
